b44828ab97be5da5c892c74196e8b123149fc134
Alvin He <alvinhe9@gmail.com>
2025-02-07 14:57:39 +1100

    made progress on homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,42 +5,48 @@ import React, { useState } from 'react';
 
 function App() {
 
-
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to track menu open/close
-
-  const openMenu = () => {
-    setIsMenuOpen(true); // Set the menu to open
-  };
-
-  const closeMenu = () => {
-    setIsMenuOpen(false); // Set the menu to close
-  };
-
-
-
-
+  // useState returns two things a variable for the initial state in this case set to false and a function to change the variable
+  const [isChecked, setIsChecked] = useState(false);
+  
+  // create a function that change setIsChecked to its opposite value not necesariy though as you can just do this by hand
+  const handleChange = () => {
+    setIsChecked(!isChecked)
+  }
 
 
   return (
     <div className="App">
       <header className="header">
-        <label className="burger" htmlFor="burger">
-          <input 
-            type="checkbox"
-            id="burger" 
-            checked={isMenuOpen} // Tie the checkbox to state
-            onChange={() => setIsMenuOpen(!isMenuOpen)} // Toggle state when clicked
-          />
-          <span />
-          <span />
-          <span />
+        <label className="menuCheckBox">
+          <input type="checkbox" checked={isChecked} onChange={handleChange} />
+          <label className="side-menu">
+            <button className = "btn1">Login</button>
+            <button className = "btn2">Search</button>
+          </label>
         </label>
         <div className="logo">Angie's Food Blog</div>
       </header>
       <div className="featured">
-        <div className="overlay"></div>
-        <button className="btn">Featured Recipes</button>
+        <div className = "featuredTextBox"></div>
+        <div className="featuredText">Featured Recipes</div>
+        <div className="featuredTextDescription">"A recipe is a story that ends with a good meal."</div>
       </div>
+      <div className="options">
+        <div className = "savoury">
+          <div className='whiteTextBox'></div>
+        </div>
+        <div className = "sweet">
+          <div className='whiteTextBox'></div>
+        </div>
+        <div className = "travel">
+          <div className='whiteTextBox'></div>
+        </div>
+      </div>
+      <div className="banner">
+        <div className="quote">"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment."</div>
+      </div>
+      <div className="aboutMe"></div>
+      <div className="banner"></div>
     </div>
   );
 }
